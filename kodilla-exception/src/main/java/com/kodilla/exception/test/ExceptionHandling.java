27c5b9044e3cfb7259652af8d5bf41e102d38151
anna-partyka <ankapartyka@o2.pl>
2018-02-23 23:29:34 +0100

    Second Challenge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -5,9 +5,11 @@ public class ExceptionHandling{
 
         SecondChallenge secondChallenge = new SecondChallenge();
         try {
-            String result = secondChallenge.probablyIWillThrowException(1.5, 1.5);
-        } catch (Exception e){
+            String result = secondChallenge.probablyIWillThrowException(3111.5, 1.5);
+            System.out.println(result);
 
+        } catch (Exception e){
+            System.out.println("It will be always an Exception");
         }finally{
             System.out.println("Second Challenge");
         }
